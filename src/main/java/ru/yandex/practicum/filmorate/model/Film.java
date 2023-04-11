4553c067977bc38bdce0438bcf25c725b7556e92
Gleb <gleb.verbickiy@yandex.ru>
2023-04-11 15:17:26 +0300

    sprint 9

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -3,27 +3,19 @@ package ru.yandex.practicum.filmorate.model;
 import lombok.Builder;
 import lombok.Data;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Positive;
-import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 @Data
-@Builder(toBuilder = true)
+@Builder
 public class Film {
 
     private int id;
 
-    @NotBlank(message = "Поле с названием фильма должно быть заполнено")
     private String name;
 
-    @NotBlank
-    @Size(min = 1, max = 200, message = "Количество символов должно быть больше 0 и не превышать 200")
     private String description;
 
-    @NotNull(message = "Поле дата релиза должно быть заполнено")
     private LocalDate releaseDate;
-    @Positive(message = "Продолжительность не может быть отрицательной")
+
     private int duration;
 }
--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -9,20 +9,16 @@ import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 @Data
-@Builder(toBuilder = true)
+@Builder
 public class User {
 
     private int id;
 
-    @NotBlank(message = "Поле с email должно быть заполнено")
-    @Email(message = "Поле с email должно содержать @")
     private String email;
 
-    @NotNull(message = "Поле с логином должно быть заполнено")
-    @NotBlank(message = "Поле с логином не должно содержать пробелы")
     private String login;
 
     private String name;
-    @NotNull(message = "Поле с датой рождения должно быть заполнено")
+
     private LocalDate birthday;
 }
